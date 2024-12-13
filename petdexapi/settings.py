ef039b79a013bd6507c21532eeb348b1f88f41b9
Matías Peralta <matias.peralta@velaindustriesgroup.com>
2024-12-13 03:24:55 -0300

    change to debug mode

--- petdexapi/settings.py
+++ petdexapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*^%no96q^qme!y#gfjz*s-%g@fobs19r3h!*c@n1+m8(6jjx1c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
